8a72282474ac3c1718a1bc877554f07de71c70fe
Aman Chitransh <aman.chitransh@sap.com>
2022-02-02 00:20:29 +0530

    Practice Day 3

--- src/com/company/DayOne.java
+++ src/com/company/DayOne.java
@@ -1,9 +1,28 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 public class DayOne {
     public static void main(String[] args) {
-        System.out.print("Aman");
+        System.out.println("___________________________________________________________");
+        System.out.println("Enter the marks for percentage calculation...");
+        System.out.println("___________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the marks for first subject: ");
+        float sub1 = sc.nextFloat();
+        System.out.println("Enter the marks for second subject: ");
+        float sub2 = sc.nextFloat();
+        System.out.println("Enter the marks for third subject: ");
+        float sub3 = sc.nextFloat();
+        System.out.println("Enter the marks for fourth subject: ");
+        float sub4 = sc.nextFloat();
+        System.out.println("Enter the marks for fifth subject: ");
+        float sub5 = sc.nextFloat();
 
+        float per = ((sub1+sub2+sub3+sub4+sub5)/500)*100;
+        System.out.println("___________________________________________________________");
+        System.out.println("                   Percentage is:"+per);
+        System.out.println("___________________________________________________________");
     }
 }
