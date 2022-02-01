e07e7939573d240486e893f0c9e20521038efc11
Aman Chitransh <69617654+amanchitransh@users.noreply.github.com>
2022-02-02 00:46:36 +0530

    Practice Day 3 (#1)
    Co-authored-by: Aman Chitransh <aman.chitransh@sap.com>

---
+++ src/com/company/Day_Three.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Day_Three {
+    public static void main(String[] args) {
+        float v=4.0f;
+        float u =5.0f;
+        float a =2.0f;
+        float s =3.0f;
+        float qwes = (v*v-u*u)/(2*a*s);
+        System.out.println(qwes);
+
+    }
+}
